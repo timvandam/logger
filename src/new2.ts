fcf95b43dff39f66d12746f48326fb94ad7f512f
Tim van Dam <timvandamcs@gmail.com>
2021-02-06 21:40:36 +0100

    log level creation

--- src/new2.ts
+++ src/new2.ts
@@ -6,8 +6,8 @@ interface Message {}
 type LogArgument = string | number | bigint | Record<string, unknown>
 type LogFunction<T extends Record<string, unknown> = Record<string, unknown>> = (...args: LogArgument[]) => Message & T
 
-type BaseLogMessage = { message: string } & Record<string, unknown>
-const baseLog: LogFunction<BaseLogMessage> = (...args: LogArgument[]): Message & BaseLogMessage => {
+type BaseLogMessage = { message: string }
+const baseLog: LogFunction<BaseLogMessage> = <Z extends LogArgument[]>(...args: Z): Message & BaseLogMessage => {
 	const msg = { message: '' }
 
 	for (const arg of args) {
@@ -26,15 +26,8 @@ const baseLog: LogFunction<BaseLogMessage> = (...args: LogArgument[]): Message &
 
 	msg.message = msg.message.slice(0, -1)
 
-	return msg
+	return msg as Message & BaseLogMessage & CombinedObjects<Z>
 }
-
-type CombinedObjects<
-	T extends readonly unknown[],
-	S extends Record<string, unknown> = Record<string, never>
-> = T extends readonly []
-	? S
-	: CombinedObjects<SliceFirst<T>, First<T> extends Record<string, unknown> ? S & First<T> : S>
 
 type TimestampLogMessage = { timestamp: Date }
 const timestampLog: LogFunction<TimestampLogMessage> = (): Message & TimestampLogMessage => ({
@@ -51,27 +44,47 @@ const levels = ['error', 'warn', 'info', 'http', 'verbose', 'debug', 'silly'] as
 type LogLevelMessage = { level: typeof levels[number] }
 const logLevelLog: LogFunction<LogLevelMessage> = () => ({ level: 'info' }) // default level is info
 
-function getLogLevelLoggers(log: LogFunction): Record<typeof levels[number], LogFunction> {
-	const loggers: Record<string, LogFunction> = {}
-
-	for (const level of levels) loggers[level] = (...args: LogArgument[]) => ({ ...log(...args), level })
-
-	return loggers
-}
-
 type ExtendedMessage<T extends readonly LogFunction[], S extends Message = Message> = T extends readonly []
 	? S
 	: ExtendedMessage<SliceFirst<T>, S & ReturnType<First<T>>>
 
-function compose<T extends readonly LogFunction[]>(...loggers: T) {
-	return <Z extends readonly LogArgument[]>(...args: Z): ExtendedMessage<T> & CombinedObjects<Z> => {
-		return loggers.reduce((msg, logger) => ({ ...msg, ...logger(...args) }), {}) as ExtendedMessage<T> &
+type CombinedObjects<T extends readonly unknown[], S extends Record<string, unknown> = {}> = T extends readonly []
+	? S
+	: CombinedObjects<SliceFirst<T>, First<T> extends Record<string, unknown> ? S & First<T> : S>
+
+function compose<T extends readonly LogFunction[], P extends Partial<ExtendedMessage<T>>>(loggers: T, override?: P) {
+	return <Z extends readonly LogArgument[]>(...args: Z): ExtendedMessage<T> & CombinedObjects<Z> =>
+		loggers.reduce((msg, logger) => ({ ...msg, ...logger(...args), ...override }), {}) as ExtendedMessage<T> &
 			CombinedObjects<Z>
+}
+
+type ListWithAtLeastOne<L extends readonly unknown[], Y, C extends readonly unknown[] = L> = C extends readonly []
+	? readonly [Y, ...L]
+	: First<C> extends Y
+	? L
+	: ListWithAtLeastOne<L, Y, SliceFirst<C>>
+
+function createLogLevels<
+	Z extends ListWithAtLeastOne<typeof loggers, LogFunction<LogLevelMessage>>,
+	P extends Partial<ExtendedMessage<Z>>
+>(loggers: Z) {
+	return {
+		silly: compose(loggers, { level: 'silly' } as P),
+		info: compose(loggers, { level: 'info' } as P),
+		http: compose(loggers, { level: 'http' } as P),
+		verbose: compose(loggers, { level: 'verbose' } as P),
+		error: compose(loggers, { level: 'error' } as P),
+		warn: compose(loggers, { level: 'warn' } as P),
+		debug: compose(loggers, { level: 'debug' } as P),
 	}
 }
 
-const log = compose(baseLog, timestampLog, pidLog, logLevelLog)
-const { error, debug, http, info, silly, verbose, warn } = getLogLevelLoggers(log)
-const l = silly('hello', 'world', { a: 'b' }, { b: 'c' })
+const loggers = [baseLog, timestampLog, logLevelLog, pidLog] as const
+const log = compose(loggers)
 
+log('asd', 'asd', { a: 1 })
+
+const { silly } = createLogLevels(loggers)
+
+const l = silly('hello', 'world', { a: 'b' }, { b: 'c' })
 console.log(l)
